bb8f61de8846323d5ee8ebc933042755f671bafb
Andrew Crerar <andrew@crerar.io>
2017-12-09 23:45:49 -0500

    Refactoring per suggestions from code review

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,20 @@ use std::char;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 2 || (&args[1]).len() == 0 {
-        println!("Usage: ./rust-rot47 <text_to_encode/decode>");
+    if args.len() != 2 || args[1].len() == 0 {
+        println!("Usage: ./rust-rot47 <encode/decode text>");
         return;
     }
 
-    let text = &args[1];
-    let mut translation = String::new();
+    println!("{}", (&args[1]).chars().map(translate).collect::<String>());
+}
 
-    for letter in text.chars() {
-        let code_point = letter as u32;
+fn translate(ch: char) -> char {
+    let code_point = ch as u32;
 
-        if code_point >= 33 && code_point <= 126 {
-            let c = char::from_u32(33 + ((code_point + 14) % 94));
-            translation.push(c.unwrap());
-        } else {
-            translation.push(letter);
-        }
+    if code_point >= 33 && code_point <= 126 {
+        return char::from_u32(33 + ((code_point + 14) % 94)).unwrap();
     }
 
-    println!("{}", translation);
+    ch
 }
